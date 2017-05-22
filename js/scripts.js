2310f044552ac3096e04061adb5a4ee364f8a010
MAttia Zirpoli <nighteye@new-host-2.station>
2017-05-22 15:43:10 +0200

    Completed the necessary features

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,68 @@
-var example_channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-var baseURL = 'https://wind-bow.gomix.me/twitch-api/'
+var exampleChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+var baseURL = 'https://wind-bow.gomix.me/twitch-api/';
+var endURL = '?callback=?';
+var windowWidth = $(window).width();
 
 $(document).ready(function() {
-  $.each(example_channels, function(index, value) {
-    getChannelInfo(value);
-    console.log(value);
+  $.each(exampleChannels, function(index, value) {
+    createChannelElement(value);
   });
 });
 
-function createListElement(listJSON) {
-  $("#users-list").append(formListString(listJSON));
-}
-
-function formListString(jsonObject) {
-  if(jsonObject.avatar === null) {
-    jsonObject.avatar = "https://static-cdn.jtvnw.net/jtv-static/404_preview-1920x1080.png";
-  }
-
-  return "<li class='list-group-item clearfix'> \
-    <img src='" + jsonObject.avatar + "' class='avatar img-rounded'/> \
-    <div class='channel-info'> \
-      <h3 class='channel-name'><a href=" + jsonObject.channel_url + ">" + jsonObject.channel_name +"</a></h3> \
-    <p class='channel-desc'>"+ jsonObject.channel_desc +"</p> \
-    </div> \
-  </li>";
-}
-
-function getChannelInfo(name) {
-  $.getJSON(baseURL + 'streams/' + name + '?callback=?', function(data) {
+/* Gets the JSON from the Twitch api and after creating a Object with the necessary
+ info it appends it to the appropriate ul lists */
+function createChannelElement(name) {
+  $.getJSON(baseURL + 'streams/' + name + endURL, function(data) {
 
     if(data.stream !== null) {
       channel = data.stream.channel;
-      // Get the useful information in a slimmer JSON object
-      var listJSON = {
+      // Get the useful information in a slimmer JS object
+      var listJSO = {
         avatar: channel.logo,
         channel_name: channel.display_name,
         channel_desc: channel.status,
         channel_url: channel.url
       }
 
-      createListElement(listJSON);
+      $("#all-list").append(formListString(listJSO));
+      $("#online-list").append(formListString(listJSO));
     }
     else {
-        $.getJSON(baseURL + 'channels/' + name + '?callback=?', function(data) {
-          console.log(data.logo)
-          var listJSON = {
-            avatar: data.logo,
-            channel_name: data.display_name,
-            channel_desc: "Offline",
-            channel_url: data.url
-          }
+      // Get the useful information in a slimmer JS object
+      $.getJSON(baseURL + 'channels/' + name + '?callback=?', function(data) {
+        var listJSO = {
+          avatar: data.logo,
+          channel_name: data.display_name,
+          channel_desc: "Offline",
+          channel_url: data.url
+        }
 
-          createListElement(listJSON);
-        });
+        $("#all-list").append(formListString(listJSO));
+        $("#offline-list").append(formListString(listJSO));
+      });
     }
-
   });
+}
+
+// Forms the appropriate string to create a new List element
+function formListString(channelInfo) {
+  if(channelInfo.avatar === null) {
+    channelInfo.avatar = "https://static-cdn.jtvnw.net/jtv-static/404_preview-1920x1080.png";
+  }
+
+  // checks if the window width is too narrow for all the characters to fit
+  // if it is, it truncates the channel description to a maximum of 25 characters
+  if(windowWidth < 768) {
+    if(channelInfo.channel_desc.length > 20) {
+      channelInfo.channel_desc = channelInfo.channel_desc.substring(0, 20) + '...';
+    }
+  }
+
+  return "<li class='list-group-item clearfix'> \
+  <img src='" + channelInfo.avatar + "' class='avatar img-rounded'/> \
+  <div class='channel-info'> \
+  <h4 class='channel-name'><a href=" + channelInfo.channel_url + ">" + channelInfo.channel_name +"</a></h4> \
+  <p class='channel-desc'>"+ channelInfo.channel_desc +"</p> \
+  </div> \
+  </li>";
 }
